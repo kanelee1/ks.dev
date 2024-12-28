fea25b8bcae4cb6fb52a053589eb698babb1ed4e
kane <74887583+kanelee1@users.noreply.github.com>
2024-12-28 14:47:58 -0800

    fixed bug where lightswitch and theme is not saved when page refreshed

--- portfolio/src/app/components/ThemeToggle.jsx
+++ portfolio/src/app/components/ThemeToggle.jsx
@@ -3,33 +3,45 @@
 import React from 'react'
 import styles from './ThemeToggle.module.css'
 import { useTheme } from 'next-themes';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Flex, Switch } from '@radix-ui/themes';
 import { SunIcon, MoonIcon } from '@radix-ui/react-icons';
 
+/**
+ * Toggles Theme componenet between light and dark mode
+ * Toggles light switch icon between Sun and Moon icon
+ * Saves current theme in local storage in case of page refresh
+ */
 const ThemeToggle = () => {
-    /**
-     * TO DO:
-     * Fix bug when refreshing theme on light mode doesnt save lightswitch state
-     * use localStorage with the useEffect hook
-     */
     const [lightSwitch, setLightSwitch] = useState(false);
     const {resolvedTheme, setTheme} = useTheme();
 
+    // set initial state of theme based on localStorage
+    useEffect(() => {
+        const storedTheme = localStorage.getItem('theme');
+        if (storedTheme) {
+            // if storedTheme !null, set that as current theme/set light switch
+            setTheme(storedTheme);
+            setLightSwitch(storedTheme === 'light');    // sets lightswitch to true if default theme is 'light', false otherwise
+        } else {
+            // if no theme stored, set 'dark' to default
+            const defaultTheme = resolvedTheme === 'dark' ? 'dark' : 'light'; // 
+            setTheme(defaultTheme);
+            setLightSwitch(defaultTheme === 'light') 
+        }
+    }, [resolvedTheme, setTheme])
+
     // toggle state of lightSwitch
     const handleLightSwitch = () => {
-        setLightSwitch(prevState => !prevState); 
-        handleToggleTheme();
-    };
-
-    // toggle light and dark theme
-    const handleToggleTheme = () => {
-        setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+        const newTheme = resolvedTheme === 'dark' ? 'light' : 'dark';
+        setTheme(newTheme);
+        localStorage.setItem('theme', newTheme);    // store new theme into local storage
+        setLightSwitch(prevState => !prevState);    // update switch state
     };
 
     return (
         <Flex className={styles.lightSwitch} gap="2">
-            <Switch color='blue'  onClick={() => handleLightSwitch()} />
+            <Switch color='blue' onClick={handleLightSwitch} checked={lightSwitch} />
             {lightSwitch ? <SunIcon /> : <MoonIcon />}
         </Flex>
     )
