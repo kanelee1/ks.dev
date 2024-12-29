5f7707a8356d5e4047ed8c6eecb17c71bcd2d895
kane <74887583+kanelee1@users.noreply.github.com>
2024-12-29 15:42:33 -0800

    replaced separator, misc. styling

--- portfolio/src/app/page.jsx
+++ portfolio/src/app/page.jsx
@@ -1,4 +1,4 @@
-import { Badge, Flex, Heading, Separator, Text, Card } from "@radix-ui/themes";
+import { Badge, Flex, Heading, Text, Card } from "@radix-ui/themes";
 import { LinkedInLogoIcon, GitHubLogoIcon, DiscordLogoIcon, EnvelopeClosedIcon, ArrowRightIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import styles from "./page.module.css";
@@ -47,36 +47,38 @@ export default function Home() {
             <Flex gapY="2" direction="column">
               <Heading size="4" weight="light">Technical Skills:</Heading>
               <Flex gap="2" className={styles.badgeContainer}>
-                <Badge radius="full" color="green" size="3">JavaScript</Badge>
-                <Badge radius="full" color="green" size="3">C++</Badge>
-                <Badge radius="full" color="green" size="3">HTML/CSS</Badge>
-                <Badge radius="full" color="green" size="3">SQL</Badge>
+                <Badge variant="soft" radius="full" color="amber" size="3" className={styles.badge}>JavaScript</Badge>
+                <Badge variant="soft" radius="full" color="amber" size="3" className={styles.badge}>C++</Badge>
+                <Badge variant="soft" radius="full" color="amber" size="3" className={styles.badge}>HTML/CSS</Badge>
+                <Badge variant="soft" radius="full" color="amber" size="3" className={styles.badge}>SQL</Badge>
               </Flex>
 
               <Flex gap="2" className={styles.badgeContainer}>
-                <Badge radius="full" color="red" size="3">Git</Badge>
-                <Badge radius="full" color="red" size="3">MongoDB</Badge>
-                <Badge radius="full" color="red" size="3">MySQL</Badge>
-                <Badge radius="full" color="red" size="3">AWS</Badge>
-                <Badge radius="full" color="red" size="3">GCP</Badge>
+                <Badge variant="soft" radius="full" color="tomato" size="3" className={styles.badge}>Git</Badge>
+                <Badge variant="soft" radius="full" color="tomato" size="3" className={styles.badge}>Vercel</Badge>
+                <Badge variant="soft" radius="full" color="tomato" size="3" className={styles.badge}>MongoDB</Badge>
+                <Badge variant="soft" radius="full" color="tomato" size="3" className={styles.badge}>MySQL</Badge>
+                <Badge variant="soft" radius="full" color="tomato" size="3" className={styles.badge}>AWS</Badge>
+                <Badge variant="soft" radius="full" color="tomato" size="3" className={styles.badge}>GCP</Badge>
               </Flex>
               
               <Flex gap="2" className={styles.badgeContainer}>
-                <Badge radius="full" color="indigo" size="3">React.js</Badge>
-                <Badge radius="full" color="indigo" size="3">Next.js</Badge>
-                <Badge radius="full" color="indigo" size="3">Node.js</Badge>
+                <Badge variant="soft" radius="full" color="indigo" size="3" className={styles.badge}>React.js</Badge>
+                <Badge variant="soft" radius="full" color="indigo" size="3" className={styles.badge}>React Native</Badge>
+                <Badge variant="soft" radius="full" color="indigo" size="3" className={styles.badge}>Next.js</Badge>
+                <Badge variant="soft" radius="full" color="indigo" size="3" className={styles.badge}>Node.js</Badge>
               </Flex>
 
               <Flex gap="2" className={styles.badgeContainer}>
-                <Badge radius="full" color="orange" size="3">RadixUI</Badge>
-                <Badge radius="full" color="orange" size="3">Motion</Badge>
+                <Badge variant="soft" radius="full" color="orange" size="3" className={styles.badge}>RadixUI</Badge>
+                <Badge variant="soft" radius="full" color="orange" size="3" className={styles.badge}>Framer Motion</Badge>
               </Flex>
   
             </Flex>
           </Flex>
         </section>
 
-        <Separator className={styles.separator} size="2"/>
+        <Flex className={styles.separator} />
 
         <section id="projects" className={styles.sectionContainer}>
           <Flex gapY="5" direction="column">
@@ -85,12 +87,12 @@ export default function Home() {
             <Flex gap="7" className={styles.cardContainer}>
               <Card size="3" variant="ghost" className={styles.card}>
                 <Flex gapY="2" direction="column">
-                  <Heading size="5" weight="light">PIC Health PWA</Heading>
+                  <Heading size="4" weight="regular">PIC Health PWA</Heading>
                   <Flex gap="1" className={styles.badgeContainer}>
-                    <Badge radius="full" color="green" size="2">JavaScript</Badge>
-                    <Badge radius="full" color="green" size="2">HTML/CSS</Badge>
-                    <Badge radius="full" color="blue" size="2">React Native</Badge>
-                    <Badge radius="full" color="red" size="2">Google Calendar API</Badge>
+                    <Badge variant="soft" radius="full" color="amber" size="2" className={styles.badge}>JavaScript</Badge>
+                    <Badge variant="soft" radius="full" color="amber" size="2" className={styles.badge}>HTML/CSS</Badge>
+                    <Badge variant="soft" radius="full" color="blue" size="2" className={styles.badge}>React Native</Badge>
+                    <Badge variant="soft" radius="full" color="tomato" size="2" className={styles.badge}>Google Calendar API</Badge>
                   </Flex>
                   <ul className={styles.listContainer}>
                     <li><Text color="gray" weight="light" className={styles.listItemText}>Event and health resource management tool developed for the Pacific Islander Health organization and CSUSM Capstone Project.</Text></li>
@@ -102,11 +104,11 @@ export default function Home() {
   
               <Card size="3" variant="ghost" className={styles.card}>
                 <Flex gap="2" direction="column">
-                  <Heading size="5" weight="light">SeniorSync</Heading>
+                  <Heading size="4" weight="regular">SeniorSync</Heading>
                   <Flex gap="1" className={styles.badgeContainer}>
-                    <Badge radius="full" color="green" size="2">JavaScript</Badge>
-                    <Badge radius="full" color="green" size="2">HTML/CSS</Badge>
-                    <Badge radius="full" color="red" size="2">Pocketbase API</Badge>
+                    <Badge variant="soft" radius="full" color="amber" size="2" className={styles.badge}>JavaScript</Badge>
+                    <Badge variant="soft" radius="full" color="amber" size="2" className={styles.badge}>HTML/CSS</Badge>
+                    <Badge variant="soft" radius="full" color="tomato" size="2" className={styles.badge}>Pocketbase API</Badge>
                   </Flex>
                   <ul className={styles.listContainer}>
                     <li><Text color="gray" weight="light" className={styles.listItemText}>Web app enabeling family members of senior citizens to monitor their relatives&apos; health and activities.</Text></li>
@@ -118,12 +120,12 @@ export default function Home() {
   
               <Card size="3" variant="ghost" className={styles.card}>
                 <Flex gap="2" direction="column">
-                  <Heading size="5" weight="light">Student Forum</Heading>
+                  <Heading size="4" weight="regular">Student Forum</Heading>
                   <Flex gap="1" className={styles.badgeContainer}>
-                    <Badge radius="full" color="green" size="2">JavaScript</Badge>
-                    <Badge radius="full" color="green" size="2">HTML/CSS</Badge>
-                    <Badge radius="full" color="green" size="2">PHP</Badge>
-                    <Badge radius="full" color="red" size="2">MySQL</Badge>
+                    <Badge variant="soft" radius="full" color="amber" size="2" className={styles.badge}>JavaScript</Badge>
+                    <Badge variant="soft" radius="full" color="amber" size="2" className={styles.badge}>HTML/CSS</Badge>
+                    <Badge variant="soft" radius="full" color="amber" size="2" className={styles.badge}>PHP</Badge>
+                    <Badge variant="soft" radius="full" color="tomato" size="2" className={styles.badge}>MySQL</Badge>
                   </Flex>
                   <ul className={styles.listContainer}>
                     <li><Text color="gray" weight="light" className={styles.listItemText}>Reddit-style web forum tailored for students to create threads, comment on posts and search through boards.</Text></li>
@@ -132,46 +134,51 @@ export default function Home() {
                   </ul>
                 </Flex>
               </Card>
-              <a href="/resume.pdf" className={styles.anchor} target="_blank" rel="noopener noreferrer">
-                <Text className={styles.anchorText} weight="light">View Full Resume</Text>
-                <ArrowRightIcon className={styles.arrowRightIcon}/>
-              </a>
+              <Flex>
+                <a href="/resume.pdf" className={styles.anchor} target="_blank" rel="noopener noreferrer">
+                  <Text className={styles.anchorText} weight="regular">View Full Resume</Text>
+                  <ArrowRightIcon className={styles.arrowRightIcon}/>
+                </a>
+              </Flex>
             </Flex>
           </Flex>
-
-
         </section>
 
-        <Separator className={styles.separator} size="2"/>
+        <Flex className={styles.separator} />
 
         <section id="contact" className={styles.sectionContainer}>
           <Flex gapY="5" direction="column">
             <Heading size="5" weight="medium">CONTACT</Heading>
-            <Text>
-              If you&apos;d like to get in touch, please feel free to reach out using the following methods.
-            </Text>
+            <Flex gapY="1" direction="column">
+              <Text weight="light" color="gray">
+                If you&apos;d like to get in touch, please feel free to reach out using the following methods.
+              </Text>
+              <Text weight="light" color="gray">
+                My preferred method of contact is through email or LinkedIn.
+              </Text>
+            </Flex>
             <Flex direction="column">
               <Heading size="4" weight="medium">Email</Heading>
               <a href="mailto:svela002@csusm.edu" className={styles.contactAnchor} target="_blank" rel="noopener noreferrer">
-                <Text>svela002@csusm.edu</Text>
+                <Text color="gray" weight="light">svela002@csusm.edu</Text>
               </a>
             </Flex>
             <Flex direction="column">
               <Heading size="4" weight="medium">LinkedIn</Heading>
               <a href="https://www.linkedin.com/in/kane-svelan/" className={styles.contactAnchor} target="_blank" rel="noopener noreferrer">
-                <Text>linkedin.com/in/kane-svelan/</Text>
+                <Text color="gray" weight="light">linkedin.com/in/kane-svelan/</Text>
               </a>
             </Flex>
             <Flex direction="column">
               <Heading size="4" weight="medium">GitHub</Heading>
               <a href="https://github.com/kanelee1" className={styles.contactAnchor} target="_blank" rel="noopener noreferrer">
-                <Text>github.com/kanelee1</Text>
+                <Text color="gray" weight="light">github.com/kanelee1</Text>
               </a>
             </Flex>
             <Flex direction="column">
               <Heading size="4" weight="medium">Discord</Heading>
               <a href="https://discord.com/users/376672204261425154" className={styles.contactAnchor} target="_blank" rel="noopener noreferrer">
-                <Text>svela002</Text>
+                <Text color="gray" weight="light">svela002</Text>
               </a>
             </Flex>
           </Flex>
